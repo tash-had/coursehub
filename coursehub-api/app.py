1b7783715c8bca62021cccba1ba6c58555ce878f
Tash-had Saqif <me@tash-had.com>
2018-10-20 01:04:08 -0400

    setup server

---
+++ coursehub-api/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, abort, request
+
+app = Flask(__name__)
+URL_PREFIX = "/api/v1.0"
+
+
+@app.route('/')
+def home():
+    abort(403)
+
+
+@app.route(URL_PREFIX + '/hello')
+def test_endpoint():
+    return jsonify({"msg":"hello world",
+                    "data": [1, 2, 3, 4, 5, 6]})
+
+
+@app.route(URL_PREFIX + '/login')
+def not_so_secure_login():
+    """ Login a user 
+    Request parameters: email, password 
+
+    :return dict: the given credentials
+    """
+    username, password = request.json.get("email"), request.json.get("password")
+    return jsonify({"username": username,
+                    "password": password})
+
+if __name__ == '__main__':
+    app.run(debug=True)
