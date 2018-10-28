b043d55d51ca579d60fc05c607b3fe548be60671
Nathan Chao <ndchao@telus.net>
2018-10-27 20:40:57 -0400

    added function prototypes for comment classes

---
+++ coursehub-api/Comment/Comment.py
@@ -0,0 +1,54 @@
+import datetime
+
+
+class Comment:
+    """
+    An object that is a comment on a Course.
+
+    Attributes:
+    ===========
+    @type score: int
+    @type text: str
+    @type time_stamp: datetime.datetime
+    @type courseID: int
+    @type ID: int
+    @rtype: None
+
+    """
+
+    def __init__(self, score, text, time_stamp, course_id, comment_id):
+        """
+        @type score: int
+        @type text: str
+        @type time_stamp: datetime.datetime
+        @type course_id: int
+        @type comment_id: int
+        @rtype: None
+
+        """
+        self.score = score
+        self.text = text
+        self.time_stamp = time_stamp
+        self.course_id = course_id
+        self.comment_id = comment_id
+
+    def get_score(self):
+        return self.score
+
+    def set_score(self, score):
+        self.score = score
+
+    def get_time_stamp(self):
+        return self.time_stamp
+
+    def get_course(self):
+        pass
+
+    def set_course(self, course):
+        pass
+
+
+
+
+
+
