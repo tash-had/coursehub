ce3104acbeefa8889e2a23e24fbf1346e87e27b1
Labib <labibc01@gmail.com>
2018-10-30 22:15:37 -0400

    basic searchbar functionality implemented

--- mdb-angular-free-6.2.6/src/app/components/searchbar/searchbar.component.ts
+++ mdb-angular-free-6.2.6/src/app/components/searchbar/searchbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SearchbarService} from './searchbar.service';
 
 @Component({
   selector: 'app-searchbar',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./searchbar.component.scss']
 })
 export class SearchbarComponent implements OnInit {
-
-  constructor() { }
+  COURSE_MATCHER = new RegExp("^[a-zA-Z]{3}[0-9]{1,3}$");
+  course : string;
+  
+  // $(document).ready(function () {
+  //     searchDropdown = new SearchDropdown();
+  
+  //     $('#courseSearch').on('input', function (e) {
+  //         var input = this.value;
+  //         if (input && input.length > 3 && COURSE_MATCHER.test(input)) {
+  //             var URL = SEARCH_COURSE_API + input;
+  //             $.get(URL, function (response) {
+  //                 searchDropdown.initSearchDialog(response);
+  //             });
+  //             return true;
+  //         }
+  //         return false;
+  //     });
+  // });
+  constructor(private searchbarService: SearchbarService) { }
 
   ngOnInit() {
+  }
+
+  checkValidInput(event: any) : boolean{
+    if (this.course && this.course.length > 3  && this.COURSE_MATCHER.test(this.course)){
+      console.log(this.course);
+    }
+
+    return true;
   }
 
 }
