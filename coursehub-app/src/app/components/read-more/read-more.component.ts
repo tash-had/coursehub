eb14b3286644fc366dd7b06e8e8cdf3f6c189107
Tash-had Saqif <me@tash-had.com>
2018-11-04 01:07:18 -0500

    finish implementation to add a readme button so all course cards in search results are teh same size

---
+++ coursehub-app/src/app/components/read-more/read-more.component.ts
@@ -0,0 +1,50 @@
+/**
+ * CREDIT : https://archive.is/XqrEE
+ */
+import { Component, Input, ElementRef, OnChanges} from '@angular/core';
+
+@Component({    
+    selector: 'read-more',
+    template: `
+        <div [innerHTML]="currentText">
+        </div>
+            <a [class.hidden]="hideToggle" (click)="toggleView()" id="readMoreToggle">Read {{isCollapsed? 'more':'less'}}</a>`,
+    styleUrls: ['./read-more.component.scss']
+})
+
+export class ReadMoreComponent implements OnChanges {
+    @Input() text: string;
+    @Input() maxLength: number = 100;
+    currentText: string;
+    hideToggle: boolean = true;
+
+    public isCollapsed: boolean = true;
+
+    constructor(private elementRef: ElementRef) {
+
+    }
+
+    toggleView() {
+        this.isCollapsed = !this.isCollapsed;
+        this.determineView();
+    }
+
+    determineView() {
+        if (!this.text || this.text.length <= this.maxLength) {
+            this.currentText = this.text;
+            this.isCollapsed = false;
+            this.hideToggle = true;
+            return;
+        }
+        this.hideToggle = false;
+        if (this.isCollapsed == true) {
+            this.currentText = this.text.substring(0, this.maxLength) + "...";
+        } else if(this.isCollapsed == false)  {
+            this.currentText = this.text;
+        }
+
+    }
+    ngOnChanges() {
+        this.determineView();       
+    }
+}
