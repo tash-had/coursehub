5d57b07c0d147655ef85a7b3fcb41a522e83dea9
Tash-had Saqif <me@tash-had.com>
2018-11-23 01:04:47 -0500

    update to fix prod issues with paths and https

--- coursehub-app/src/app/views/dashboard/dashboard.component.ts
+++ coursehub-app/src/app/views/dashboard/dashboard.component.ts
@@ -43,10 +43,10 @@ export class DashboardComponent implements AfterViewInit {
   toggleTheme(blueTheme: boolean) {
     if (blueTheme) {
       this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'white';
-      this.coursehubLogo.nativeElement.src = "../../assets/Courshub-blue.png";
+      this.coursehubLogo.nativeElement.src = "/assets/Courshub-blue.png";
     } else {
       this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'black';
-      this.coursehubLogo.nativeElement.src = "../../assets/Courshub.png";
+      this.coursehubLogo.nativeElement.src = "/assets/Courshub.png";
     }
   }
 
