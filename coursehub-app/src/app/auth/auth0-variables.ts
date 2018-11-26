73577703bf5b8bdd344b72a56e4c490ced85b750
Tash-had Saqif <me@tash-had.com>
2018-11-26 15:51:40 -0500

    Update auth0-variables.ts

--- coursehub-app/src/app/auth/auth0-variables.ts
+++ coursehub-app/src/app/auth/auth0-variables.ts
@@ -7,5 +7,5 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: '3B3L1WG7c9WyVhi117UJqXSiOmfLWoH7',
   domain: 'coursehub.auth0.com',
-  callbackURL: 'http://localhost:4200/callback'
+  callbackURL: 'https://coursehub.ca/callback'
 };
