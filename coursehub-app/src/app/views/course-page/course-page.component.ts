094e027f0b73018985f5219a399cdccd83b8e994
Labib <labibc01@gmail.com>
2018-11-27 18:45:01 -0500

    added rating sliders

--- coursehub-app/src/app/views/course-page/course-page.component.ts
+++ coursehub-app/src/app/views/course-page/course-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  ElementRef, ViewChild } from '@angular/core';
 import { CourseCardDataService } from '../../components/course-card/course-card-data.service'
 import { ActivatedRoute } from '@angular/router';
 import { CoursePageDataService } from './course-page-data.service'
@@ -11,6 +11,12 @@ import { CoursePageDataService } from './course-page-data.service'
 
 export class CoursePageComponent implements OnInit {
   courseData: Object;
+  @ViewChild("useRating") usefulRating: ElementRef;
+  @ViewChild("usefulnessSlider") usefulSlider: ElementRef;
+  @ViewChild("diffRating") diffRating: ElementRef;
+  @ViewChild("difficultySlider") difficultSlider: ElementRef;
+  usefulCourseRating: number;
+  difficultCourseRating: number;
 
   //mock comments
   comments: String[] = ['Hey, I love CSC301', 'Hey, I do not like CSC301 that much', 
@@ -19,7 +25,9 @@ export class CoursePageComponent implements OnInit {
   constructor(private courseCardDataService: CourseCardDataService, private coursePageDataService: CoursePageDataService, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.courseCardDataService.courseData.subscribe(courseData => this.courseCardDataReceived(courseData))
+    this.courseCardDataService.courseData.subscribe(courseData => this.courseCardDataReceived(courseData));
+    this.setSliders();
+
   }
 
   courseCardDataReceived(courseData: Object) {
@@ -30,10 +38,22 @@ export class CoursePageComponent implements OnInit {
     }
   }
 
+  setSliders(){
+    this.usefulCourseRating = this.courseData['ratings']['recommendation_rating'];
+    this.difficultCourseRating = this.courseData['ratings']['workload_rating'];
+    this.usefulSlider.nativeElement.value = Math.round(100*(this.usefulCourseRating / 5));
+    this.usefulRating.nativeElement.innerHTML = Math.round(100*(this.usefulCourseRating / 5));
+    this.difficultSlider.nativeElement.value = Math.round(100*(this.difficultCourseRating / 5));
+    this.diffRating.nativeElement.innerHTML = Math.round(100*(this.difficultCourseRating / 5));
+  }
+
   getCourseData() {
     const courseId = +this.route.snapshot.paramMap.get('courseId');
     this.coursePageDataService.getCourseData(courseId.toString())
-      .subscribe((courseData) => this.courseData = courseData);
+      .subscribe((courseData) => {
+        this.courseData = courseData;
+        this.setSliders();
+      });
   }
 
   addComment() {
@@ -42,4 +62,41 @@ export class CoursePageComponent implements OnInit {
         this.currentComment = null;       
     }
   }
+
+  updateRating(type: String){
+    if (type == 'usefulness'){
+      this.usefulRating.nativeElement.innerHTML = this.usefulSlider.nativeElement.value;
+    }else if (type == 'difficulty'){
+      this.diffRating.nativeElement.innerHTML = this.difficultSlider.nativeElement.value;
+    }
+    
+  }
+
+  assignRatingLevel(type: string) : string {
+    if (type == 'usefulness'){
+      var usefulNum = Math.round(100*(this.usefulCourseRating / 5));
+      if (usefulNum > 79){
+        return 'green';
+      }
+      else if (usefulNum <= 79 && usefulNum > 59){
+        return 'yellow';
+      }
+      else{
+        return 'red';
+      }
+    }
+    else if (type == 'difficulty'){
+      var difficultNum = Math.round(100*(this.difficultCourseRating / 5));
+      if (difficultNum > 79){
+        return 'green';
+        
+      }
+      else if (difficultNum <= 79 && difficultNum > 59){
+        return 'yellow';
+      }
+      else{
+        return 'red';
+      }
+    }
+  } 
 }
