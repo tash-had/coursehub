ae372b5f1117ccd8380d4b1dc59f240839c59a70
Cullen Caume <cullen.caume@mail.utoronto.ca>
2018-11-26 20:03:54 -0500

    added fucntions to comment and updated some functions according to new database and added user to comment function and user db functions

--- coursehub-api/db/workers/UserDatabaseWorker.py
+++ coursehub-api/db/workers/UserDatabaseWorker.py
@@ -6,3 +6,18 @@ class UserDatabaseWorker(DatabaseManager):
 
     def __init__(self):
         DatabaseManager.__init__(self)
+
+    def get_username_by_id(self, user_id):
+        """
+        Get the username for a specific user
+
+        :param str user_id: user we want the username for
+        :return:
+        """
+        cur = self.db_conn.cursor()
+        cur.execute("SELECT username FROM users WHERE id=?", [user_id])
+
+        results = cur.fetchall()
+        if results is None:
+            return []
+        return results
--- coursehub-api/db/workers/UserToCommentDatabaseWorker.py
+++ coursehub-api/db/workers/UserToCommentDatabaseWorker.py
@@ -7,4 +7,22 @@ class UserToCommentDatabaseWorker(DatabaseManager):
     def __init__(self):
         DatabaseManager.__init__(self)
 
-    def
+    def get_all_comments_from_user(self, user_id):
+        """
+        Get all the comments for a given user id.
+
+        :param user_id:
+        :return:
+        """
+        cur = self.db_conn.cursor()
+        cur.execute("""SELECT c.id,
+                    FROM user_to_comment_table uc
+                    INNER JOIN comments c ON uc.comment_id = c.id
+                    WHERE uc.user_id = ?""", [user_id])
+
+        results = cur.fetchall()
+        if results is None:
+            return []
+        return results
+
+
